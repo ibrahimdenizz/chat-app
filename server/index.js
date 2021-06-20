cdfaf83243dc8e8527f0f108a0a5ab5e2f07631d
ibrahim <dnzibrahim1@gmail.com>
2021-06-20 16:53:23 +0300

    Add .prittierrc and format index.js

--- server/index.js
+++ server/index.js
@@ -1,23 +1,23 @@
-const cors = require('cors');
-const users = require('./routers/users');
-const auth = require('./routers/auth');
-const mongoose = require('mongoose');
-const config = require('config');
-const express = require('express');
-const path = require('path');
+const cors = require("cors");
+const users = require("./routers/users");
+const auth = require("./routers/auth");
+const mongoose = require("mongoose");
+const config = require("config");
+const express = require("express");
+const path = require("path");
 const app = express();
-const http = require('http').Server(app);
-const io = require('socket.io')(http, {
+const http = require("http").Server(app);
+const io = require("socket.io")(http, {
   cors: {
-    origin: config.get('url'),
-    methods: ['GET', 'POST'],
+    origin: config.get("url"),
+    methods: ["GET", "POST"],
   },
 });
 
-const uri = config.get('db_uri') + '/chatApp';
+const uri = config.get("db_uri") + "/chatApp";
 
 const corsOptions = {
-  origin: config.get('url'),
+  origin: config.get("url"),
   optionSuccessStatus: 200,
 };
 
@@ -27,96 +27,96 @@ mongoose
     useUnifiedTopology: true,
   })
   .then(() => {
-    console.log('Connect Db');
+    console.log("Connect Db");
   });
 
 app.use(express.json());
 app.use(cors(corsOptions));
-app.use('/api/users', users);
-app.use('/api/auth', auth);
+app.use("/api/users", users);
+app.use("/api/auth", auth);
 
 let msgId = 0;
 const onlineUsers = [];
 
-io.on('connection', (socket) => {
-  console.log('connect');
+io.on("connection", (socket) => {
+  console.log("connect");
   let currentUser;
 
-  socket.join('Public 1');
-  socket.join('Public 2');
+  socket.join("Public 1");
+  socket.join("Public 2");
 
-  socket.on('chat message', (msg) => {
-    if (msg.data == 'joined')
+  socket.on("chat message", (msg) => {
+    if (msg.data == "joined")
       currentUser = {
         username: msg.user,
         room: msg.room,
       };
     if (msg.isPrivate) {
-      console.log('Private Message : ', msg, msgId);
-      io.to(socket.id).emit('chat message', { ...msg, id: msgId });
+      console.log("Private Message : ", msg, msgId);
+      io.to(socket.id).emit("chat message", { ...msg, id: msgId });
       const receiver = msg.room;
       msg.room = socket.id;
-      io.to(receiver).emit('chat message', { ...msg, id: msgId });
+      io.to(receiver).emit("chat message", { ...msg, id: msgId });
     } else {
-      console.log('Room Message : ', msg, msgId);
-      io.to(msg.room).emit('chat message', { ...msg, id: msgId });
+      console.log("Room Message : ", msg, msgId);
+      io.to(msg.room).emit("chat message", { ...msg, id: msgId });
     }
     msgId++;
   });
 
-  socket.on('Typing', (user) => {
-    console.log('Typing : ', user);
+  socket.on("Typing", (user) => {
+    console.log("Typing : ", user);
     if (user.isPrivate) {
-      io.to(socket.id).emit('Typing', user);
+      io.to(socket.id).emit("Typing", user);
       const receiver = user.room;
       user.room = socket.id;
-      io.to(receiver).emit('Typing', user);
+      io.to(receiver).emit("Typing", user);
     } else {
-      io.to(user.room).emit('Typing', user);
+      io.to(user.room).emit("Typing", user);
     }
   });
 
-  socket.on('online user', (user) => {
+  socket.on("online user", (user) => {
     const index = onlineUsers.findIndex((a) => a.username === user.username);
     if (index === -1) {
       onlineUsers.push(user);
-      console.log('Online', user, onlineUsers);
+      console.log("Online", user, onlineUsers);
     }
   });
 
-  socket.on('get online user', (get) => {
-    console.log('Online', onlineUsers);
-    io.emit('get online user', onlineUsers);
+  socket.on("get online user", (get) => {
+    console.log("Online", onlineUsers);
+    io.emit("get online user", onlineUsers);
   });
 
-  socket.on('join room', (room) => {
+  socket.on("join room", (room) => {
     socket.join(room);
-    console.log('Join Room', socket.rooms);
+    console.log("Join Room", socket.rooms);
   });
 
-  socket.on('leave room', (room) => {
+  socket.on("leave room", (room) => {
     socket.leave(room);
-    console.log('Leave Room', socket.rooms);
+    console.log("Leave Room", socket.rooms);
     if (room && room.isPrivate) {
-      io.to(room.id).emit('chat message', {
-        data: 'left',
+      io.to(room.id).emit("chat message", {
+        data: "left",
         user: currentUser.username,
         room: socket.id,
       });
     } else if (room) {
-      io.to(room.id).emit('chat message', {
-        data: 'left',
+      io.to(room.id).emit("chat message", {
+        data: "left",
         user: currentUser.username,
         room: room.id,
       });
     }
   });
 
-  socket.on('disconnect', () => {
+  socket.on("disconnect", () => {
     if (currentUser) {
-      console.log('message : left ', currentUser, msgId);
-      io.emit('chat message', {
-        data: 'left',
+      console.log("message : left ", currentUser, msgId);
+      io.emit("chat message", {
+        data: "left",
         user: currentUser.username,
         room: currentUser.room,
         id: msgId,
@@ -126,16 +126,16 @@ io.on('connection', (socket) => {
         (a) => a.username === currentUser.username
       );
       onlineUsers.splice(index, 1);
-      io.emit('get online user', onlineUsers);
+      io.emit("get online user", onlineUsers);
     }
   });
 });
 
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('client/build'));
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
 
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
 }
 
