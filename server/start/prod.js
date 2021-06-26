371d03b781bc3cf7481739245c1d04f58515acfc
ibrahim <dnzibrahim1@gmail.com>
2021-06-26 14:53:20 +0300

    Remove helmet because of heroku blank page issue

--- server/start/prod.js
+++ server/start/prod.js
@@ -1,5 +1,4 @@
 const morgan = require("morgan");
-const helmet = require("helmet");
 const express = require("express");
 const cors = require("cors");
 const config = require("config");
@@ -10,7 +9,6 @@ const corsOptions = {
 };
 
 module.exports = function (app) {
-  app.use(helmet());
   app.use(morgan("dev"));
   app.use(express.json());
   app.use(cors(corsOptions));
