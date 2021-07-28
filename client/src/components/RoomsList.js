ec7e68bd2f8a0ad0bf98cf76fd0a6f4063ca3403
ibrahim <dnzibrahim1@gmail.com>
2021-07-28 20:41:13 +0300

    Add Css to RoomsList component

--- client/src/components/RoomsList.js
+++ client/src/components/RoomsList.js
@@ -1,22 +1,23 @@
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faTimes } from "@fortawesome/free-solid-svg-icons";
+import "./RoomsList.css";
 
 const RoomsList = ({ rooms, onChangeRoom, onLeaveRoom, activeRoom }) => {
   return (
-    <div className="container  w-100 h-75 overflow-auto p-0">
-      <ul className="list-group d-flex">
+    <div className="rooms-list-wrapper">
+      <ul className="rooms-list">
         {rooms.map((room) => (
           <li
             key={room.id}
             onClick={() => onChangeRoom(room)}
-            className={`list-group-item ${
+            className={`rooms-list-item ${
               room.id === activeRoom.id ? "active" : ""
-            } list-group-item-action btn`}
+            }`}
           >
-            <div className=" container row w-100 text-break p-0 m-0">
-              <div className="col-11 p-0">{room.name}</div>
-              <div className="btn col-1 p-0" onClick={() => onLeaveRoom(room)}>
+            <div className="content">
+              <div className="name">{room.name}</div>
+              <div className="icon" onClick={() => onLeaveRoom(room)}>
                 <FontAwesomeIcon
                   icon={faTimes}
                   style={{
